816bbe039dc95f8e4b09951d0b2bba0af911f332
OlsJacob-FS <jdols@student.fullsail.edu>
2024-11-21 18:22:47 -0500

    Added styling to page

--- App.js
+++ App.js
@@ -19,13 +19,33 @@ export default function App() {
         );
         setWeather(response.data); 
       } catch (error) {
-        console.error(error);
+        console.error("Error while featching weather data", error);
       }
-
   }
+  //Handle city input from user
+  const handleInputChange = (text) => {
+    setCity(text);
+    if(text.length > 2) {
+      fetchWeather(text);
+    }
+  }
+
   return (
     <View style={styles.container}>
-      <Text>This is it working </Text>
+      <Text style={styles.title}>Weather App</Text>
+      <TextInput
+        style={styles.input}
+        onChangeText={text => handleInputChange(text)}
+        value={city}
+        placeholder='Enter city name'
+      />
+      {weather && (
+        <View style={styles.watherContainer}>
+          <Text stytle={styles.city}>{weather.name}</Text>
+          <Text stytle={styles.temp}>{weather.main.temp}</Text>
+          <Text stytle={styles.desc}>{weather.weather[0].description}</Text>
+          </View>
+      )}
     </View>
   );
 }
@@ -33,8 +53,42 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+    padding: 20,
+    backgroundColor: '#111111',
   },
+  title: {
+    color: '#ffffff',
+    fontWeight: "bold",
+    fontSize: 28,
+    marginBottom: 20,
+  },
+  input: {
+    width: '100%',
+    height: 40,
+    padding: 10,
+    borderColor: `#ffffff`,
+    borderWidth: 1,
+    borderRadius: 10,
+    marginBottom: 20,
+    backgroundColor: '#ffffff',
+    color: '#111111',
+  },
+  weatherContainer: {
+    alignItems: 'center',
+  },
+  city: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: '#ffffff',
+
+  },
+  desc: {
+    fontSize: 16,
+    textTransform: 'capitalize',
+    color: '#ffffff',
+    
+  }
 });
+
