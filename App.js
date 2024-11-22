f0067a215b4970ea0cd1f8a13260bc32eaf5bfd3
OlsJacob-FS <jdols@student.fullsail.edu>
2024-11-21 21:49:55 -0500

    Location services being added, loading icon and weather icon added.

--- App.js
+++ App.js
@@ -1,7 +1,8 @@
 //Import react dependants 
 import React, { useState } from 'react';
-import { View, Text, TextInput, StyleSheet } from 'react-native';
+import { View, Text, TextInput, StyleSheet, Alert, TouchableOpacity, ActivityIndicator, Image } from 'react-native';
 import axios from 'axios';
+import * as Location from 'expo-location';
 import Constants from 'expo-constants';
 
 
@@ -9,46 +10,60 @@ import Constants from 'expo-constants';
 export default function App() {
 
   const [city, setCity] = useState('');
-  const [weather, setWeather] = useState('');
+  const [weather, setWeather] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
 
-  
   const fetchWeather = async () => {
-    const apiKey = Constants.manifest.extra.apiKey;
+    
+    const apiKey = Constants.expoConfig.extra.apiKey;
+    if( !city){
+      Alert.alert('Error', 'City name is required');
+    }
+    setIsLoading(true);
       try {
-        const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=metric`
-        );
+        const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`);
         setWeather(response.data); 
       } catch (error) {
         console.error("Error while featching weather data", error);
+        Alert.alert("Error", "Could not fetch weather. Check the city name")
+      } finally {
+        setIsLoading(false);
       }
-  }
-  //Handle city input from user
-  const handleInputChange = (text) => {
-    setCity(text);
-    if(text.length > 2) {
-      fetchWeather(text);
     }
-  }
-
+  //Handle city input from user
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Weather App</Text>
       <TextInput
         style={styles.input}
-        onChangeText={text => handleInputChange(text)}
+        onChangeText={(text) => setCity(text)}
         value={city}
         placeholder='Enter city name'
       />
-      {weather && (
-        <View style={styles.watherContainer}>
-          <Text stytle={styles.city}>{weather.name}</Text>
-          <Text stytle={styles.temp}>{weather.main.temp}</Text>
-          <Text stytle={styles.desc}>{weather.weather[0].description}</Text>
+      <TouchableOpacity style={styles.searchBtn} title="Search" onPress={fetchWeather}>
+        <Text>Search</Text>
+      </TouchableOpacity>
+  {isLoading ? (
+        <ActivityIndicator size="large" color="#28f5fc" style={styles.loader}/>
+      ) : (
+      weather && (
+        <View style={styles.weatherContainer}>
+          <Text style={styles.city}>{weather?.name}</Text>
+          <Text style={styles.temp}>{weather?.main.temp}°F</Text>
+          <Text style={styles.desc}>{weather?.weather[0].description}</Text>
+          <Image source={{
+            uri: `https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`
+          }}
+          style={styles.weatherIcon}
+          />
           </View>
+      )
       )}
+  
     </View>
   );
 }
+
 
 const styles = StyleSheet.create({
   container: {
@@ -68,27 +83,50 @@ const styles = StyleSheet.create({
     width: '100%',
     height: 40,
     padding: 10,
-    borderColor: `#ffffff`,
+    borderColor: '#ffffff',
     borderWidth: 1,
-    borderRadius: 10,
+    borderRadius: 8,
     marginBottom: 20,
-    backgroundColor: '#ffffff',
-    color: '#111111',
+    backgroundColor: '#fff',
   },
   weatherContainer: {
     alignItems: 'center',
+    marginTop: 20,
+    backgroundColor: '#fff',
+    padding: 20,
+    borderRadius: 15,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+  },
+  searchBtn: {
+    backgroundColor: '#28f5fc',
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 15,
+    width: "25%",
+    borderRadius: 25,
+    marginTop: 5,
   },
   city: {
     fontSize: 20,
     fontWeight: 'bold',
-    color: '#ffffff',
-
+  },
+  temp: {
+    fontSize: 20,
+    color: '#007BFF',
+    marginVertical: 10,
   },
   desc: {
     fontSize: 16,
     textTransform: 'capitalize',
-    color: '#ffffff',
-    
-  }
+  },
+  loader: {
+    marginTop: 20,
+  },
+  weatherIcon: {
+    width: 150,
+    height: 150,
+    marginTop: 10,
+  },
 });
 
--- app.config.js
+++ app.config.js
@@ -1,13 +1,12 @@
 import 'dotenv/config';
-import { version } from 'react';
 
 export default {
-    expo: {
-        name: "WeatherApp",
-        slug: "Weather-App",
-        version: "1.0.0",
-        extra: {
-            apiKey: process.env.OPEN_WEATHER_API_KEY
-        },
+  expo: {
+    name: 'WeatherApp',
+    slug: 'weatherapp',
+    version: '1.0.0',
+    extra: {
+      apiKey: process.env.OPEN_WEATHER_API_KEY,
     },
-}
+  },
+};
