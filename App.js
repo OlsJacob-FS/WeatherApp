2cf611c9642aa88c83b2d8210b7a14b77f3ab9be
OlsJacob-FS <jdols@student.fullsail.edu>
2024-11-23 21:29:05 -0500

    Added gps location service with minor styling of components

--- App.js
+++ App.js
@@ -5,28 +5,61 @@ import axios from 'axios';
 import * as Location from 'expo-location';
 import Constants from 'expo-constants';
 
-
-
 export default function App() {
 
   const [city, setCity] = useState('');
   const [weather, setWeather] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
+  const [location, setLocation] = useState(null);
 
-  const fetchWeather = async () => {
-    
+  const fetchWeather = async (coords = null) => {
     const apiKey = Constants.expoConfig.extra.apiKey;
-    if( !city){
-      Alert.alert('Error', 'City name is required');
+    let url;
+
+    //Check if long and lat are available
+    if(coords){
+      const {latitude, longitude} = coords;
+      url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=imperial`;
+    } else if (city) {
+      url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
+    } else {
+      Alert.alert('Error', "Please enter a city name or enable location services");
+      return;
     }
+
+    // if(!city){
+    //   Alert.alert('Error', 'City name is required');
+    //   return;
+    // }
+    //Set loading to true while fetching API data
     setIsLoading(true);
       try {
-        const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`);
+        const response = await axios.get(url);
         setWeather(response.data); 
       } catch (error) {
         console.error("Error while featching weather data", error);
         Alert.alert("Error", "Could not fetch weather. Check the city name")
       } finally {
+        setIsLoading(false);
+      }
+    }
+
+    const getLocation = async () => {
+      setIsLoading(true);
+      try{
+        const { status } = await Location.requestForegroundPermissionsAsync()
+        if( status !== 'granted'){
+          Alert.alert("Permission Denied", 'Allow location access to fetch weather data.')
+          setIsLoading(false);
+          return;
+        }
+
+        const userLocation = await Location.getCurrentPositionAsync({});
+        setLocation(userLocation.coords);
+        fetchWeather(userLocation.coords);
+      } catch (error) {
+        console.error("Error while fetching location", error);
+        Alert.alert("Error", "Could not fetch location. Check location settings")
         setIsLoading(false);
       }
     }
@@ -42,6 +75,9 @@ export default function App() {
       />
       <TouchableOpacity style={styles.searchBtn} title="Search" onPress={fetchWeather}>
         <Text>Search</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.searchBtn} title="Search" onPress={getLocation}>
+        <Text>Use Location</Text>
       </TouchableOpacity>
   {isLoading ? (
         <ActivityIndicator size="large" color="#28f5fc" style={styles.loader}/>
@@ -103,7 +139,7 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     padding: 15,
-    width: "25%",
+    width: "40%",
     borderRadius: 25,
     marginTop: 5,
   },
